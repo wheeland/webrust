505183609e72c1b327ea2a28d82d1675eba72084
Wieland Hagen <wieland.hagen@kdab.com>
2019-06-07 00:07:33 +0900

    Use thy fucking wasm32 biaaaatch

--- planetator/build.sh
+++ planetator/build.sh
@@ -19,6 +19,6 @@ export RUST_BACKTRACE=1
 # set to debuginfo=1 to profile Rust code inside the browsers!
 export RUSTFLAGS='-C panic=abort -C lto -C opt-level=z -C overflow-checks=no -C debuginfo=0 -C debug-assertions=no'
 
-cargo build --target asmjs-unknown-emscripten --release
+cargo build --target wasm32-unknown-emscripten --release
 
 php -S 0.0.0.0:8080
